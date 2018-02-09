86b0b7bffc86c7f8f3bc201e8d18211b1cf04fb4
pavel.a <pavel.a@sunrise-r.ru>
2018-02-09 17:05:54 +0300

    #webpack-dev-demo added config for excluding folders from building typescript; added strings to exclude domo and node_modules folders from deletion of *.js, d.ts files

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -name "*.js" -type f -delete
-find . -name "*.js.map" -type f -delete
-find . -name "*.d.ts" -type f -delete
+find . -type f -name "*.js" ! -path "./demo/*" ! -path "./node_modules/*" -delete
+find . -type f -name "*.js.map" ! -path "./demo/*" ! -path "./node_modules/*" -delete
+find . -type f -name "*.d.ts" ! -path "./demo/*" ! -path "./node_modules/*" -delete
